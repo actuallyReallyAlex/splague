e1a627260d97a010c208367a53d87d54ce624d1a
Alex <alex@alex-lee.site>
2018-09-11 15:18:37 -0700

    Added MenuScreen view and MainSidebar component.

---
+++ src/MainSidebar.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+import PropTypes from 'prop-types'
+
+export default class MainSidebar extends Component {
+  handleItemClick = name => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state || {}
+
+    const identity = this.props.player.morality === 'good' ? 'Doctor' : 'Plague'
+
+    const semStyle = {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      bottom: 0,
+      // padding: '4px 0.5rem',
+      padding: '4px 1rem',
+      // margin: '2px',
+      // color: '#8ff',
+      // background: '#222'
+      background: '#1b1c1d',
+      width: '250px'
+    }
+
+    return (
+      <div style={semStyle}>
+        <Menu
+          fluid
+          inverted
+          vertical
+          borderless
+          compact
+          style={{ display: 'flex', flexDirection: 'column', flex: 1 }}
+        >
+          <Menu.Item>
+            <Menu.Header>
+              <h1>{this.props.player.name}</h1>
+            </Menu.Header>
+          </Menu.Item>
+
+          <Menu.Item>
+            <Menu.Header>World Information</Menu.Header>
+            <Menu.Menu>
+              <Menu.Item
+                name="Population"
+                active={activeItem === 'Population'}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                name="Plague Progression"
+                active={activeItem === 'Plague Progression'}
+                onClick={this.handleItemClick}
+              />
+              <Menu.Item
+                name="Cure Progression"
+                active={activeItem === 'Cure Progression'}
+                onClick={this.handleItemClick}
+              />
+            </Menu.Menu>
+          </Menu.Item>
+
+          <Menu.Item>
+            <Menu.Header>{identity} Information</Menu.Header>
+            <Menu.Menu>
+              <Menu.Item
+                name="Other Stuffs"
+                active={activeItem === 'Other Stuffs'}
+                onClick={this.handleItemClick}
+              />
+            </Menu.Menu>
+          </Menu.Item>
+        </Menu>
+      </div>
+    )
+  }
+}
+
+MainSidebar.propTypes = {
+  player: PropTypes.object
+}
---
+++ src/Views/MenuScreen.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import MainSidebar from '../MainSidebar'
+
+const MenuScreen = props => {
+  const styles = {
+    marginLeft: '250px'
+  }
+
+  let morality = props.player.morality
+
+  morality === 'good'
+    ? props.resetBackground()
+    : props.resetBackground('#252839')
+
+  return (
+    <div className="full-height">
+      <MainSidebar player={props.player} />
+      <div style={styles} className="full-height">
+        <h1>Hello world.</h1>
+      </div>
+    </div>
+  )
+}
+
+MenuScreen.propTypes = {
+  player: PropTypes.object,
+  resetBackground: PropTypes.func
+}
+
+export default MenuScreen
