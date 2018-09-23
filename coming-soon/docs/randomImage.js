5cfa6ed4dd86702a1bb3862b52a7fa3ac5f666ab
Alex Lee <alex@alex-lee.site>
2018-09-22 17:09:09 -0700

    Testing 3.

--- coming-soon/docs/randomImage.js
+++ coming-soon/docs/randomImage.js
@@ -1,10 +1,12 @@
 function addStyleString(str) {
-  var node = document.createElement('style');
-  node.innerHTML = str;
-  document.body.appendChild(node);
+  var node = document.createElement('style')
+  node.innerHTML = str
+  document.body.appendChild(node)
 }
 
-addStyleString('body { color: red }');
+addStyleString(
+  '.cover-main img { content: url("https://assets.pernod-ricard.com/uk/media_images/test.jpg" }'
+)
 // addStyleString('body { background: silver }');
 // This way allows you to add CSS in multiple passes
 
