07d215eef855a80194ec8c71bc72bcd5f26df61e
Alex Lee <alex@alex-lee.site>
2018-09-22 17:17:41 -0700

    If this doesn't work, you'll need to not use relative paths.

--- coming-soon/docs/randomImage.js
+++ coming-soon/docs/randomImage.js
@@ -4,20 +4,31 @@ function addStyleString(str) {
   document.body.appendChild(node)
 }
 
-const dictionary = [
-  'http://i.pravatar.cc/300?img=1',
-  'http://i.pravatar.cc/300?img=2',
-  'http://i.pravatar.cc/300?img=3',
-  'http://i.pravatar.cc/300?img=4',
-  'http://i.pravatar.cc/300?img=5',
-  'http://i.pravatar.cc/300?img=6',
-  'http://i.pravatar.cc/300?img=7',
-  'http://i.pravatar.cc/300?img=8',
-  'http://i.pravatar.cc/300?img=9',
-  'http://i.pravatar.cc/300?img=10'
+// const dictionary = [
+//   'http://i.pravatar.cc/300?img=1',
+//   'http://i.pravatar.cc/300?img=2',
+//   'http://i.pravatar.cc/300?img=3',
+//   'http://i.pravatar.cc/300?img=4',
+//   'http://i.pravatar.cc/300?img=5',
+//   'http://i.pravatar.cc/300?img=6',
+//   'http://i.pravatar.cc/300?img=7',
+//   'http://i.pravatar.cc/300?img=8',
+//   'http://i.pravatar.cc/300?img=9',
+//   'http://i.pravatar.cc/300?img=10'
+// ]
+
+const images = [
+  './images/bubonic.svg',
+  './images/earthTribe.svg',
+  './images/patientZero.svg',
+  './images/pneumonic.svg',
+  './images/septicemic.svg',
+  './images/splague.svg',
+  './images/tradesmen.svg',
+  './images/warlords.svg'
 ]
 
-let url = dictionary[Math.floor(Math.random()*dictionary.length)]
+let url = images[Math.floor(Math.random()*images.length)]
 
 addStyleString(
   `.cover-main img { content: url(${url}) }`
