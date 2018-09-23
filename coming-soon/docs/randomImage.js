1db5924364ab9ee57b73198a63ec3f949518cd09
Alex Lee <alex@alex-lee.site>
2018-09-22 17:46:43 -0700

    Lots of logic stuff.

--- coming-soon/docs/randomImage.js
+++ coming-soon/docs/randomImage.js
@@ -1,11 +1,5 @@
 // https://stackoverflow.com/questions/15505225/inject-css-stylesheet-as-string-using-javascript
 
-function addStyleString(str) {
-  var node = document.createElement('style')
-  node.innerHTML = str
-  document.body.appendChild(node)
-}
-
 const images = [
   './images/bubonic.svg',
   './images/earthTribe.svg',
@@ -16,16 +10,54 @@ const images = [
   './images/warlords.svg'
 ]
 
-let path = images[Math.floor(Math.random() * images.length)]
+function addStyleString(str) {
+  var node = document.createElement('style')
+  node.innerHTML = str
+  document.body.appendChild(node)
+}
 
-addStyleString(`.cover-main img { content: url(${path}) }`)
+/**
+ * Tests whether or not 2 strings are equal to each other.
+ * @param {String} firstString First string to test.
+ * @param {String} secondString Second string to test.
+ * @returns {Boolean} Returns true if the strings are the same, and false otherwise.
+ */
+function isTheSame(firstString, secondString) {
+  firstString === secondString ? true : false
+}
 
+/**
+ * Ensures that a new image is returned to the user.
+ * @param {Array} images An array of images to choose from.
+ * @param {String | Undefined} lastPath If defined, the path to the image that was most recently shown to the user. Otherwise, undefined.
+ * @returns {String} A string gaurunteed not to be the same path to the image the user saw most recently.
+ */
+function getRandomPath(images, lastPath) {
+  // If the user saw an image last time, i.e.
+  // the lastPath has been set in localStorage
+  if (lastPath) {
+    // Make a new path
+    let path = images[Math.floor(Math.random() * images.length)]
+
+    // If the path was the same as the lastPath
+    while (isTheSame(path, lastPath)) {
+      console.log('the path was the same as the last path!')
+      path = images[Math.floor(Math.random() * images.length)]
+    }
+
+    return path
+  } else {
+    // Make a new path
+    let path = images[Math.floor(Math.random() * images.length)]
+
+    return path
+  }
+}
+
+// If the browser supports localStorage
 if (typeof Storage !== 'undefined') {
-  // // Store current path, so that when the user refreshes, they never get the same image twice in a row
-  // localStorage.setItem("lastname", "Smith");
-  // // Retrieve
-  // document.getElementById("result").innerHTML = localStorage.getItem("lastname");
-  localStorage.setItem('alex', 'lee');
-} else {
-  // Sorry! No Web Storage support..
+  // Get a path to a random image
+  // Garunteed not to be an image the user saw most recently.
+  const path = getRandomPath(images, localStorage.getItem('lastPath'))
+  addStyleString(`.cover-main img { content: url(${path}) }`)
 }
