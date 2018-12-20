329b444118364194d95170f2be4ff3515f72aa9e
Alex Lee <alex@alex-lee.site>
2018-12-19 23:08:41 -0800

    🚀 Basic Redux setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { Grommet, grommet } from "grommet";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { Grommet, grommet } from 'grommet'
+import { Provider } from 'react-redux'
+import store from './redux/store/store'
 
 ReactDOM.render(
-  <Grommet theme={grommet}>
-    <App />
-  </Grommet>,
-  document.getElementById("root")
-);
+  <Provider store={store}>
+    <Grommet theme={grommet}>
+      <App />
+    </Grommet>
+  </Provider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
---
+++ src/redux/reducers/reducers.js
@@ -0,0 +1,12 @@
+import { LOL_BUTT } from '../actions/actions'
+
+const game = (state = [], action) => {
+  switch (action.type) {
+    case LOL_BUTT:
+      return state
+    default:
+      return state
+  }
+}
+
+export default game
