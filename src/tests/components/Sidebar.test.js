03ea82a159db9d81f3b58ce60bf99fe95f442cdd
Alex Lee <alex@alex-lee.site>
2019-01-01 09:55:18 -0800

    ✏️ Edit Sidebar test to be shallow due to connected TimeTracker child

--- src/tests/components/Sidebar.test.js
+++ src/tests/components/Sidebar.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import ShallowRenderer from 'react-test-renderer/shallow'
 import 'jest-styled-components'
 import { Sidebar } from '../../components/Sidebar'
 import developmentState from '../fixtures/developmentState'
@@ -9,19 +9,19 @@ const dispatch = jest.fn()
 describe('<Sidebar />', () => {
   it('Should render snapshot of Sidebar component.', () => {
     const { cure, log, plague, player, ui, world } = developmentState
-    const component = renderer
-      .create(
-        <Sidebar
-          cure={cure}
-          dispatch={dispatch}
-          log={log}
-          plague={plague}
-          player={player}
-          ui={ui}
-          world={world}
-        />
-      )
-      .toJSON()
-    expect(component).toMatchSnapshot()
+    const renderer = new ShallowRenderer()
+    renderer.render(
+      <Sidebar
+        cure={cure}
+        dispatch={dispatch}
+        log={log}
+        plague={plague}
+        player={player}
+        ui={ui}
+        world={world}
+      />
+    )
+    const result = renderer.getRenderOutput()
+    expect(result).toMatchSnapshot()
   })
 })
