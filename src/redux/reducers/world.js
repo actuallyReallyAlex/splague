6ce455652eab24e3eb148553d4dbd59572a84e2e
Alex Lee <alex@alex-lee.site>
2019-01-01 09:54:55 -0800

    ✏️ Ability to set dayLength and increase day

--- src/redux/reducers/world.js
+++ src/redux/reducers/world.js
@@ -2,16 +2,20 @@
 
 const worldReducerInitialState = {
   alivePopulation: 450000000,
+  day: 0,
+  dayLength: 12000,
   deadPopulation: 0,
   healthyPopulation: 450000000,
-  infectedPopulation: 0
+  infectedPopulation: 0,
 }
 
 const worldReducerDevelopmentState = {
   alivePopulation: 450000000,
+  day: 0,
+  dayLength: 12000,
   deadPopulation: 0,
   healthyPopulation: 450000000,
-  infectedPopulation: 0
+  infectedPopulation: 0,
 }
 
 let worldState
@@ -31,6 +35,16 @@ export default (state = worldState, action) => {
           action.payload.numberToInfect),
         infectedPopulation: (state.infectedPopulation +=
           action.payload.numberToInfect)
+      }
+    case 'INCREASE_DAY':
+      return {
+        ...state,
+        day: state.day + 1
+      }
+    case 'SET_DAY_LENGTH':
+      return {
+        ...state,
+        dayLength: 12000 / action.payload.dayLength
       }
     default:
       return state
