0fc45c8c8da1c7562f963f5d7e07c5ab94f54251
Alex Lee <alex@alex-lee.site>
2019-01-09 07:40:08 -0800

    ✏️ More refactoring of deconstructContinentState()

--- src/utilities.js
+++ src/utilities.js
@@ -151,12 +151,74 @@ const filterPredicate = (unwantedElement, element, elementIndex) => {
 }
 
 /**
+ * Counts the number of manual dots that need to be created.
+ * @param {Number} i Current index of element in the parent reduce function.
+ * @param {Array} arr Source array in the parent reduce function.
+ * @returns {Number} Number of manual dots to be created.
+ */
+const countNumberOfManualDots = (i, arr) => {
+  let numberOfManualDots = 0
+  // Count how many dots need to be created manually before the next real dot
+  // 100 is just an arbitrary number larger than any number of manual dots you'll ever encounter
+  for (let j = 0; j < 100 && j + i < arr.length; j++) {
+    if (isNaN(arr[i + j][0])) {
+      numberOfManualDots++
+    } else {
+      // Once you encounter an array that is a true dot, stop incrementing the number of manual dots.
+      break
+    }
+  }
+  return numberOfManualDots
+}
+
+/**
+ * Creates an array of manual dots.
+ * @param {Number} numberOfManualDots Number of manual dots to create.
+ * @param {Number} i Current index of element in the parent reduce function.
+ * @param {Array} arr Source array in the parent reduce function.
+ * @returns {Array} An array of manual dots.
+ */
+const createManualDots = (numberOfManualDots, i, arr) => {
+  const newDotArray = []
+  for (let k = 1; k < numberOfManualDots + 1; k++) {
+    // The last true dot in this row\
+    const baseDot = arr[i - 1]
+
+    // Make a new array where the first element is the base dot's first element plus 10
+    newDotArray.push([baseDot[0] + k * 10, baseDot[1]])
+  }
+  return newDotArray
+}
+
+/**
+ * Replaces placeholder dots ([NaN, 0]) with a manual dot.
+ * @param {Array} result Accumulator in the parent reduce function.
+ * @param {Array} element The current element in the parent reduce function. Will be either a "true dot" - [200, 10], or an array that represents a placeholder for a true dot - [NaN, 0].
+ * @param {Number} i Index of element in the parent reduce function.
+ * @param {Array} arr Source array in the parent reduce function.
+ * @returns {Array} Result of placeholders being replaced with manual dots.
+ */
+const replacePlaceholders = (result, element, i, arr) => {
+  if (isNaN(element[0])) {
+    // This is a dot that needs to be added manually
+    const numberOfManualDots = countNumberOfManualDots(i, arr)
+    // Now that we know how many dots need to be added manually,
+    // create an array for each one
+    const arrayOfManualDots = createManualDots(numberOfManualDots, i, arr)
+    arrayOfManualDots.forEach(newDotArray => result.push(newDotArray))
+  } else {
+    // This is a true dot
+    result.push(element)
+  }
+  return result
+}
+
+/**
  * Deconstructs an svg string into an array of coordinates.
  * @param {Object} continent Continent to work with. Given by Grommet.
  * @returns An array of coordinates, corresponding to each dot on the map of the continent.
  */
 export const deconstructContinentState = continent => {
-  // Full string as an svg path.
   return buildContinentState(continent)
     .split(' ')
     .filter(element => filterPredicate('h0', element))
@@ -165,36 +227,10 @@ export const deconstructContinentState = continent => {
       const splitUp = element.split(',')
       return [parseInt(splitUp[0]), parseInt(splitUp[1])]
     })
-    .reduce((result, element, i, arr) => {
-      if (isNaN(element[0])) {
-        // This is a dot that needs to be added manually
-        let numberOfManualDots = 0
-        // Count how many dots need to be created manually before the next real dot
-        // 100 is just an arbitrary number larger than any number of manual dots you'll ever encounter
-        for (let k = 0; k < 100 && k + i < arr.length; k++) {
-          if (isNaN(arr[i + k][0])) {
-            numberOfManualDots++
-          } else {
-            // Once you encounter an array that is a true dot, stop incrementing the number of manual dots.
-            break
-          }
-        }
-
-        // Now that we know how many dots need to be added manually,
-        // create an array for each one
-        for (let l = 1; l < numberOfManualDots + 1; l++) {
-          // The last true dot in this row\
-          const baseDot = arr[i - 1]
-
-          // Make a new array where the first element is the base dot's first element plus 10
-          const newDotArray = [baseDot[0] + l * 10, baseDot[1]]
-          result.push(newDotArray)
-        }
-      } else {
-        result.push(element)
-      }
-      return result
-    }, [])
+    .reduce(
+      (result, element, i, arr) => replacePlaceholders(result, element, i, arr),
+      []
+    )
     .filter(element => filterPredicate(0, element, 1))
     .map(element => {
       const x = element[0] / FACTOR
