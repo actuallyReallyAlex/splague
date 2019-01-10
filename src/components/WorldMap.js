c1b9280846b64e4e935f2ec3f50aa23c2a8ef66f
Alex Lee <alex@alex-lee.site>
2019-01-10 04:52:27 -0800

    ✏️ Update files, tests, snapshots

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -17,62 +17,33 @@ const theme = {
   }
 }
 
-const buildPlaces = continent => {
-  const places = []
-  const continentCoordinates = deconstructContinentState(continent)
-  for (let i = 0; i < continentCoordinates.length; i++) {
-    const place = {
-      name: `testPlace-${i}`,
-      color: 'accent-1',
-      location: continentCoordinates[i]
-    }
-    places.push(place)
-  }
-  return places
-}
-
 class WorldMap extends Component {
   state = {
     places: []
   }
 
-  handleSelectPlace = place => {
-    console.log(place)
-    const { places } = this.state
-    const newPlaces = []
-
-    places.forEach(place => {
-      newPlaces.push(place)
-    })
-
-    const newPlace = { color: 'accent-1', location: place }
-    if (
-      newPlaces.findIndex(obj => {
-        return obj === newPlace
-      }) === -1
-    ) {
-      newPlaces.push(newPlace)
+  buildPlaces = continent => {
+    const places = []
+    const continentCoordinates = deconstructContinentState(continent)
+    for (let i = 0; i < continentCoordinates.length; i++) {
+      const place = {
+        name: `testPlace-${i}`,
+        color: 'accent-1',
+        location: continentCoordinates[i]
+      }
+      places.push(place)
     }
-
-    this.setState(() => ({ places: newPlaces }))
+    return places
   }
 
   render() {
     const { places } = this.state
     return (
-      <Box
-        align="center"
-        justify="center"
-        pad="medium"
-        // style={{ border: 'red solid 3px' }}
-        width="70%"
-      >
+      <Box align="center" justify="center" pad="medium" width="70%">
         <GrommetWorldMap
           a11yTitle="WorldMap"
-          continents={[{ name: 'Asia', color: 'blue' }]}
           onSelectPlace={this.handleSelectPlace}
           places={places}
-          // style={{ border: 'red solid 3px' }}
           theme={theme}
         />
       </Box>
