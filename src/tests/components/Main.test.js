d659eb5d2c18bec46d4a8d8c90e93f82da5586bf
Alex Lee <alex@alex-lee.site>
2019-01-16 16:50:21 -0800

    🌐 35 - Redux Continent
    Adds Continents info to Redux store. Also updates population numbers to reflect that of Google searches on 01/16/2019.

--- src/tests/components/Main.test.js
+++ src/tests/components/Main.test.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import ShallowRenderer from 'react-test-renderer/shallow'
 import 'jest-styled-components'
 import Main from '../../components/Main'
 
 describe('<Main />', () => {
   it('Should render snapshot of Main component.', () => {
-    const component = renderer.create(<Main />).toJSON()
-    expect(component).toMatchSnapshot()
+    const renderer = new ShallowRenderer()
+    renderer.render(<Main />)
+    const result = renderer.getRenderOutput()
+    expect(result).toMatchSnapshot()
   })
 })
--- src/tests/components/WorldMap.test.js
+++ src/tests/components/WorldMap.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import 'jest-styled-components'
-import WorldMap from '../../components/WorldMap'
+import { WorldMap } from '../../components/WorldMap'
 
 describe('<WorldMap />', () => {
   it('Should render snapshot of WorldMap component.', () => {
