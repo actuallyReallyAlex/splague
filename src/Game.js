45bfa922d2c5a39ece71f5a0adab53253780249e
Alex Lee <alex@alex-lee.site>
2019-02-17 08:43:26 -0800

    ✏️ Small change to the visible state shown in testing screen.

--- src/Game.js
+++ src/Game.js
@@ -3,10 +3,27 @@ import { connect } from 'react-redux'
 import './logic/world'
 
 const Game = ({ dispatch, world }) => {
+  const importantState = world
+
+  const continentNames = [
+    'Africa',
+    'Asia',
+    'Australia',
+    'Europe',
+    'NorthAmerica',
+    'SouthAmerica'
+  ]
+
+  // Remove coordinates for now. This doesn't change.
+  for (let i = 0; i < continentNames.length; i++) {
+    const currentContinent = continentNames[i]
+    delete importantState.continents[currentContinent].coordinates
+  }
+
   return (
     <div>
       <h2>World State</h2>
-      <pre>{JSON.stringify(world, null, 2)}</pre>
+      <pre>{JSON.stringify(importantState, null, 2)}</pre>
     </div>
   )
 }
