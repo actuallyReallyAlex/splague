765cb7f5e68032e5bac9c05160781cc134510091
alexlee-dev <alex@alexlee.dev>
2020-06-15 10:56:36 -0700

    ✨ Use useInterval() and Save State Every 5 seconds

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,32 +1,55 @@
 import * as React from "react";
 import LoadingIndicator from "./components/LoadingIndicator";
+import useInterval from "./hooks/useInterval";
 
 /**
  * Application.
  */
 const App: React.SFC<{}> = () => {
-  const [isLoading, setIsLoading] = React.useState(true);
-
   const [money, setMoney] = React.useState(10.0);
   const [item1Count, setItem1Count] = React.useState(0);
   const [item1Cost, setItem1Cost] = React.useState(1.0);
   const [itemMultiplier, setItemMultiplier] = React.useState(1);
 
+  const [isLoading, setIsLoading] = React.useState(true);
+
   React.useEffect(() => {
     setIsLoading(false);
   }, []);
 
+  /**
+   * Earnings Interval
+   */
+  useInterval(() => {
+    const earnings = item1Count * 0.1;
+    const newMoney = money + earnings;
+    setMoney(() => Math.round(newMoney * 100) / 100);
+  }, 1000);
+
+  /**
+   * Save Game State
+   */
+  useInterval(() => {
+    localStorage.setItem(
+      "state",
+      JSON.stringify({ money, item1Count, item1Cost, itemMultiplier })
+    );
+  }, 5000);
+
+  // /**
+  //  * Initialize Game State
+  //  */
   React.useEffect(() => {
-    const earningsInterval = setInterval(() => {
-      const earnings = item1Count * 0.1;
-      const newMoney = money + earnings;
-      console.log({ money, earnings, newMoney });
-
-      setMoney(() => Math.round(newMoney * 100) / 100);
-    }, 1000);
-
-    return () => clearInterval(earningsInterval);
-  }, [money]);
+    if (localStorage.getItem("state")) {
+      console.log("RETRIEVED STATE");
+      const loadedState = JSON.parse(localStorage.getItem("state"));
+      console.log(loadedState);
+      setMoney(loadedState.money);
+      setItem1Count(loadedState.item1Count);
+      setItem1Cost(loadedState.item1Cost);
+      setItemMultiplier(loadedState.itemMultiplier);
+    }
+  }, []);
 
   return (
     <div id="app">
---
+++ src/client/hooks/useInterval.ts
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+const useInterval = (callback: Function, delay: number) => {
+  const savedCallback: React.MutableRefObject<any> = React.useRef();
+
+  // Remember the latest callback.
+  React.useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  React.useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
+
+export default useInterval;
