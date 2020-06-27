fea61198a1f857191a2fe5590691fe056cec707b
alexlee-dev <alex@alexlee.dev>
2020-06-27 15:02:13 -0700

    💬 Create Chat Component

--- src/client/actions/TreatPatient.tsx
+++ src/client/actions/TreatPatient.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import { setAlert, setPatientTreatment } from "../redux/actions";
+import Chat from "./treatments/Chat";
 import {
   Operation,
   Remedy,
@@ -118,30 +119,7 @@ const TreatPatient: React.SFC<TreatPatientProps> = (
       text: "Perform Operation",
     },
     {
-      dialogContent: (
-        <>
-          <p className="title" id="treatment-dialog-title">
-            Chat
-          </p>
-          <p>TREATMENT DIALOG CONTEXT</p>
-          <menu className="dialog-menu">
-            <button
-              className="nes-btn is-primary"
-              id="treatment-dialog-primary"
-              onClick={() => null}
-            >
-              PRIMARY
-            </button>
-            <button
-              className="nes-btn"
-              id="treatment-dialog-secondary"
-              onClick={() => null}
-            >
-              SECONDARY
-            </button>
-          </menu>
-        </>
-      ),
+      dialogContent: <Chat />,
       handler: () => {
         handleTreatmentSelect("chat");
         const treatmentDialog = document.getElementById(
--- src/client/actions/treatments/Chat.tsx
+++ src/client/actions/treatments/Chat.tsx
@@ -9,9 +9,28 @@ export interface ChatProps {
 const Chat: React.SFC<ChatProps> = (props: ChatProps) => {
   const { theme } = props;
   return (
-    <div>
-      <span>CHAT</span>
-    </div>
+    <>
+      <p className="title" id="treatment-dialog-title">
+        Chat
+      </p>
+      <p>TREATMENT DIALOG CONTEXT</p>
+      <menu className="dialog-menu">
+        <button
+          className="nes-btn is-primary"
+          id="treatment-dialog-primary"
+          onClick={() => null}
+        >
+          PRIMARY
+        </button>
+        <button
+          className="nes-btn"
+          id="treatment-dialog-secondary"
+          onClick={() => null}
+        >
+          SECONDARY
+        </button>
+      </menu>
+    </>
   );
 };
 
