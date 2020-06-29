9735d2c49da0343297774e1d2c1daea61a1ecbd0
alexlee-dev <alex@alexlee.dev>
2020-06-28 21:55:38 -0700

    💻 One Screen - Add 'setCurrentPage()' to 'MenuAction'

--- src/client/redux/actions/menu.ts
+++ src/client/redux/actions/menu.ts
@@ -1,7 +1,12 @@
-import { SET_IS_VISIBLE } from "../actionTypes";
-import { MenuAction } from "../../types";
+import { SET_CURRENT_PAGE, SET_IS_VISIBLE } from "../actionTypes";
+import { MenuAction, MenuPage } from "../../types";
 
 export const setIsVisible = (isVisible: boolean): MenuAction => ({
   type: SET_IS_VISIBLE,
   payload: { isVisible },
+});
+
+export const setPage = (page: MenuPage): MenuAction => ({
+  type: SET_CURRENT_PAGE,
+  payload: { page },
 });
--- src/client/redux/reducers/menu.ts
+++ src/client/redux/reducers/menu.ts
@@ -1,4 +1,5 @@
 import { MenuAction, MenuState } from "../../types";
+import { SET_CURRENT_PAGE, SET_IS_VISIBLE } from "../actionTypes";
 
 export const initialState: MenuState = {
   currentPage: "stats",
@@ -7,6 +8,12 @@ export const initialState: MenuState = {
 
 const reducer = (state = initialState, action: MenuAction): MenuState => {
   switch (action.type) {
+    case SET_CURRENT_PAGE: {
+      return { ...state, currentPage: action.payload.currentPage };
+    }
+    case SET_IS_VISIBLE: {
+      return { ...state, isVisible: action.payload.isVisible };
+    }
     default:
       return state;
   }
