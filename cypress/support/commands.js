20c1c53e8ef6647bb9b6a97048205f2271b7a1de
alexlee-dev <alex@alexlee.dev>
2020-06-28 23:58:18 -0700

    💻 One Screen - Add 'onboard' Command for Cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -7,6 +8,22 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
+
+Cypress.Commands.add("onboard", () => {
+  cy.get("#story").should("have.text", "Welcome to Splague!");
+  cy.get("#story-0").click();
+  cy.get("#story").should(
+    "have.text",
+    "You are a level headed doctor of medicine living in Western Europe. Above all else, you desire to help others. The year is 1345."
+  );
+  cy.get("#story-1").click();
+  cy.get("#story").should(
+    "have.text",
+    "... the Black Plague starts in 1346. Good luck."
+  );
+  cy.get("#story-2").click();
+  cy.get("#story").should("have.text", "12 months before death...");
+});
 //
 //
 // -- This is a parent command --
